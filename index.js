445d4148ec724fb7a2e83ecd7cdc391400662f70
AntoniouIoannis <antoniouioannis10@gmail.com>
2023-09-02 23:21:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+function getGRshortdate() {
+    let monthGR = ['Ιαν', 'Φεβ', 'Μαρ', 'Απρ', 'Μαΐ', 'Ιουν', 'Ιουλ', 'Αυγ', 'Σεπ', 'Οκτ', 'Νοε', 'Δεκ'];
+
+    let cdate = new Date();
+    
+    let month = monthGR[cdate.getMonth()];
+    let year = cdate.getFullYear();
+   
+    /*  εδω θα δωσουμε τη μορφη της ημερομηνιας οπως την θελουμε για τις διαρκειες ετους Εργασιες / Δεξιοτητες / Σπουδές*/
+    return month + '-' + year;
+   
+}
+
+let headerVar = document.querySelector('.header');
+headerVar.innerHTML = getGRshortdate();
+
+const oftenUsedMail = document.getElementById('oftenUsed');
+oftenUsedMail.textContent = 'Συχνά χρησιμοποιημένο e-mail';
+// εδω προαιρετικα μπορουμε να δουμε οτι εχουμε τι δυνατοτητα με alert
+alert(oftenUsedMail.textContent);
+
+function().onclick ,  function().onhover  {
+{
+
+    operator:  btnround
+        case button1  .left-side bg-color: blue .personal-info bg-color: aqua
+        case button2   .left-side bg-color: hue .personal-info bg-color: chocolate
+        case button3
+        case button4
+
+
+
+        operator:  btnrect
+        case button5 text: font-family: arial
+        case button6  text: font-family: helvetica
+        case button7
+        case button8    
+
+}
+}
+
